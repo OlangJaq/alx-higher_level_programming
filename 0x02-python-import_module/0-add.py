725acb8ab70facf9883c2dbdd129635f04f3bd4d
OlangJaq <olangjack@yahoo.com>
2023-03-09 21:07:43 +0300

    tasks

--- 0x02-python-import_module/0-add.py
+++ 0x02-python-import_module/0-add.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
+if __name__ == "__main__":
+    """Print sum of 1 and 2"""
 
-print("{:d} + {:d} = {:d}".format(a, b, result))
-
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
