d671346b30e29e968e8ddff6e89d044e160b979e
OlangJaq <olangjack@yahoo.com>
2023-03-09 19:27:00 +0300

    task 0

---
+++ 0x02-python-import_module/calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """ My addition function """
+    print(int(a) + int(b))
+
+# Get the values of a and b from user input
+a = 1
+b = 2
+
+# Call the function with the user input as arguments
+add(a, b)
