8ed88733b3ba89b97b94d5f9bb80f1599794f38c
OlangJaq <olangjack@yahoo.com>
2023-03-15 22:35:09 +0300

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,5 @@
 #!usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    """
-    Computes the square value of all integers of a matrix.
-
-    :param matrix: 2 dimensional array
-    :return: a new matrix with each value being
-    the square of the value of the input
-    """
-
-    # Create a new matrix with the same size as the input matrix
     new_matrix = [[x ** 2 for x in row] for row in matrix]
     return(new_matrix)
