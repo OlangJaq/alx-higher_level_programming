44af437fba99c8c2b655888038ea9b5762904a55
OlangJaq <olangjack@yahoo.com>
2023-03-15 22:30:04 +0300

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -10,5 +10,5 @@ def square_matrix_simple(matrix=[]):
     """
 
     # Create a new matrix with the same size as the input matrix
-    new_matrix = [[x**2 for x in row] for row in matrix]
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
     return(new_matrix)
