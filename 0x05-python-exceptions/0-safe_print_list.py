65e9edc24ae6fe3d33c659943c5a2649d7203a73
Jack Olang <117734328+OlangJaq@users.noreply.github.com>
2023-03-27 14:49:54 +0000

    task 0 rechecked

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
+            print("{}".format(my_list[i]), end=' ')
             count += 1
 
     except IndexError:
-        pass
+        break
     finally:
-        print()
+        print('')
     return count
