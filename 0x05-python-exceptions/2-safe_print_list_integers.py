e70ee792199b89a1fa8d0593b13d26abc4b085bc
Jack Olang <117734328+OlangJaq@users.noreply.github.com>
2023-04-02 15:16:54 +0000

    uncommited

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,14 +4,12 @@ def safe_print_list_integers(my_list=[], x=0):
     """ a function that prints the first x elements
     of a list and only integers."""
     count = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end=' ')
-            count += 1
-
-    except IndexError:
-        pass
-    finally:
-        print()
-        return count
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
