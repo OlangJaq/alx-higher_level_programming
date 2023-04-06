dceb768d64db352e339384149da9f4e8a743da20
Jack Olang <117734328+OlangJaq@users.noreply.github.com>
2023-04-06 19:00:53 +0000

    task 0 changed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,9 +13,9 @@ def add_integer(a, b=98):
     try:
         # Check if a and b are integers or floats
         if not isinstance(a, (int, float)):
-            raise TypeError("a must be an integer or a float")
+            raise TypeError("a must be an integer")
         if not isinstance(b, (int, float)):
-            raise TypeError("b must be an integer or a float")
+            raise TypeError("b must be an integer")
 
         # Cast a and b to integers if they are floats
         a = int(a) if isinstance(a, float) else a
